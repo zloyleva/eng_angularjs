1a26fb908b1c93f2a802c1c5d2b33dc7c3635add
oleh <oleh@devlop.loc>
2017-06-01 23:25:15 +0300

    add select lessons

--- app.js
+++ app.js
@@ -15,27 +15,48 @@
     $scope.result = '';
     $scope.trans = "";
 
+    $scope.vocab = [];
+    $scope.lesson_4 = false;
+    $scope.lesson_5 = false;
+
     /**
     * Get words
     */
-    $http.get('words.json')
-      .then(function(data) {
-        $scope.vocab = data.data;
-      });
+    $scope.changeVocab = function () {
+      $scope.vocab = [];
+      if($scope.lesson_4){
+        $http.get('words/lesson_4.json')
+          .then(function(data) {
+            $scope.vocab = data.data.concat($scope.vocab);
+          });
+      }
+      if($scope.lesson_5){
+        $http.get('words/lesson_5.json')
+          .then(function(data) {
+            $scope.vocab = data.data.concat($scope.vocab);
+          });
+      }
+    }
+
+    // $http.get('words.json')
+    //   .then(function(data) {
+    //     $scope.vocab = data.data;
+    //   });
 
     // $scope.question =
     function checkInput() {
+        if($scope.vocab.length > 0){
+          if($scope.displayAnswer.toLowerCase() == $scope.answer.toLowerCase()){
+            $scope.result = 'Correct';
+          }else {
+            $scope.result = 'Wrong, correct: "' + $scope.displayAnswer + '". Tour answer: '+ $scope.answer;
+          }
+          randomNumber = Math.floor(Math.random() * $scope.vocab.length);
+          $scope.displayQuestion = $scope.vocab[randomNumber].rus;
+          $scope.displayAnswer = $scope.vocab[randomNumber].eng;
 
-        if($scope.displayAnswer.toLowerCase() == $scope.answer.toLowerCase()){
-          $scope.result = 'Correct';
-        }else {
-          $scope.result = 'Wrong, correct: "' + $scope.displayAnswer + '". Tour answer: '+ $scope.answer;
+          $scope.trans = $scope.vocab[randomNumber].trans;
         }
-        randomNumber = Math.floor(Math.random() * $scope.vocab.length);
-        $scope.displayQuestion = $scope.vocab[randomNumber].rus;
-        $scope.displayAnswer = $scope.vocab[randomNumber].eng;
-
-        $scope.trans = $scope.vocab[randomNumber].trans;
     }
 
     $scope.foo = function (event) {
